bcd3e119a637c5f2330e7b5a8d55789201eb19fd
Anton Khomchenko <khomchenko.anton@yandex.ru>
2022-09-10 09:58:18 +0300

    fix: sprint_2

--- src/utils/validation/inputValidator.ts
+++ src/utils/validation/inputValidator.ts
@@ -4,11 +4,5 @@ export const inputValidator = (name: string, value: string) => {
   const regExpsArr = Object.entries(regExs)
   const regExpArr = regExpsArr.filter(([key, ]) => key === name).flat();
   const regEx = regExpArr[1];
-  console.log(regExpArr)
-  if (typeof regEx !== 'string' && regEx.test(value)) {
-    return true
-  }
-  else {
-    return false
-  }
+  return typeof regEx !== 'string' && regEx.test(value)
 }
