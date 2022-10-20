0b6f9534e8accc5fa529371ababe1ee21b7b03f7
Anton Khomchenko <khomchenko.anton@yandex.ru>
2022-10-20 18:45:50 +0300

    Sprint_4/iteration 2

--- webpack.common.js
+++ webpack.common.js
@@ -1,72 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-    entry: './src/index.ts',
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new CopyPlugin({
-            patterns: [
-                {
-                    from: './src/static/',
-                    to: path.resolve(__dirname, 'dist/static'),
-                },
-            ],
-        }),
-        new Dotenv()
-    ],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    resolve: {
-        extensions: ['.ts', '.js', '.json', '.svg'],
-        alias: {
-            handlebars: 'handlebars/dist/handlebars.min.js',
+  entry: './src/index.ts',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/static/',
+          to: path.resolve(__dirname, 'dist/static'),
         },
+      ],
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json', '.svg'],
+    alias: {
+      handlebars: 'handlebars/dist/handlebars.min.js',
     },
-    module: {
-        rules: [
-            {
-                test: /\.hbs$/,
-                loader: 'handlebars-loader',
-            },
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.ts?$/,
-                use: [
-                    {
-                        loader: 'ts-loader',
-                        options: {
-                            configFile: path.resolve(__dirname, 'tsconfig.json'),
-                        },
-                    },
-                ],
-                exclude: /(node_modules)/,
-            },
-            {
-                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-                type: 'asset/inline',
-            },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.hbs$/,
+        loader: 'handlebars-loader',
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
         ],
-    },
+      },
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
 };
